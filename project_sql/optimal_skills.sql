c59790f30fe8baff73bfb087fafa3023ff32a708
daniakl <daniabc4@hotmail.com>
2025-08-03 18:00:44 -0400

    new queries

---
+++ project_sql/optimal_skills.sql
@@ -0,0 +1,6 @@
+/***What are the most optimal skills to learn (aka it’s in high demand and a high-paying skill) for a data analyst?** 
+
+- Identify skills in high demand and associated with high average salaries for Data Analyst roles
+- Concentrates on remote positions with specified salaries
+- Why? Targets skills that offer job security (high demand) and financial benefits (high salaries), offering strategic insights for career development in data analysis*/
+
